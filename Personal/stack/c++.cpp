4170f8a25cea66566ae6d4c815668450ba6e6c76
Pratik Mulik <120821308+pm0049@users.noreply.github.com>
2023-09-30 00:14:25 +0530

    Update c++.cpp

--- Personal/stack/c++.cpp
+++ Personal/stack/c++.cpp
@@ -1,62 +1,59 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #define MAX 10
 using namespace std;
 
-int top, stk[MAX];
+int top = -1;
+int stk[MAX];
 
-int empty(){
-    if(top == -1)
-        return 1;
-    else
-        return 0;
-    return 0;
-}
-
-
-int full(){
-    if(top == MAX - 1)
+int empty() {
+    if (top == -1)
         return 1;
     else
         return 0;
 }
 
-int push(){
-    if(full())
+int full() {
+    if (top == MAX - 1)
+        return 1;
+    else
+        return 0;
+}
+
+int push() {
+    if (full())
         cout << "Stack is FULL!!" << endl;
-    else{
-    cout << "Enter the element to be added!" << endl;
-    cin >> stk[top];
+    else {
+        cout << "Enter the element to be added!" << endl;
+        cin >> stk[++top];
     }
 
     return 0;
 }
 
-int pop(){
+int pop() {
     if (empty())
         cout << "Stack is EMPTY!!!" << endl;
-    else{
-        cout << "Deleted element is: " + stk[top];
+    else {
+        cout << "Deleted element is: " << stk[top];
         top--;
     }
     return 0;
 }
 
-int display(){
-    for (int i = 0; i < MAX; i++)
-        cout << stk[i];    
+int display() {
+    for (int i = 0; i <= top; i++)
+        cout << stk[i] << " ";
+    cout << endl;
     return 0;
-
 }
 
-main(){
+int main() {
     int choice;
 
-    while (5)
-    {
-        cout << "1. Push\n2. Pop\n3. Display\n4.Exit\n\nEnter Choice: ";
+    while (choice != 4) {
+        cout << "1. Push\n2. Pop\n3. Display\n4. Exit\n\nEnter Choice: ";
         cin >> choice;
-        switch (choice)
-        {
+        switch (choice) {
         case 1:
             push();
             break;
@@ -68,11 +65,10 @@ main(){
             break;
         case 4:
             exit(0);
-        
         default:
             cout << "Invalid Choice!!" << endl;
         }
     }
-    
 
+    return 0;
 }
