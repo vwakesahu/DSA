1cfb478fb1d57a3d99314712bdb5bfe8538eef36
Vivek Sahu <viveksahu1762@gmail.com>
2023-12-24 08:48:25 +0000

    Working but not submitted due to time limit

---
+++ longestPalindrome.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+string reverseString(string str)
+{
+    int length = str.length();
+    string ans(length, ' '); 
+
+    for (int i = length - 1, j = 0; i >= 0; i--, j++)
+    {
+        ans[j] = str[i];
+    }
+    return ans;
+}
+
+int main()
+{
+    string q = "cabab", str = "", reverse, ans;
+    int ansLength = 0;
+
+    for (int i = 0; i < q.length(); i++)
+    {
+        for (int j = i; j < q.length(); j++)
+        {
+            str += q[j];
+            reverse = reverseString(str);
+            if (str == reverse)
+            {
+                if (str.length() > ansLength)
+                {
+                    ans = str;
+                    ansLength = ans.length();
+                }
+            }
+        }
+        str = "";
+    }
+    cout << ans;
+}
